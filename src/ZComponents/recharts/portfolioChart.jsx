ce9b2d0d444ecd2d27ecda72f33e5e5bba63ead7
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2022-11-10 20:26:01 +0530

    push lockin

--- src/ZComponents/recharts/portfolioChart.jsx
+++ src/ZComponents/recharts/portfolioChart.jsx
@@ -52,173 +52,212 @@ const datac = [
 
 export default function portfolioChart(props) {
   let data = props.data
-  let vd=props.vd
-  let vs=parseInt(props.vs)
-  let totalp=parseInt(vd?.substring(0,2));
-  if(vd?.substring(2,3)=='1'){
-    vs+=12;
+  let vd = props.vd
+  let variables = props.variables
+
+  let vs = parseInt(props.vs)
+  let totalp = parseInt(vd?.substring(0, 2));
+  if (vd?.substring(2, 3) == '1') {
+    vs += 12;
   }
-  let period=parseInt(vd?.substring(0,2));
-  let cliff=parseInt(vd?.substring(2,3));
-  if(vd?.substring(2,3)=='1'){
-    period-=12;
+  let period = parseInt(vd?.substring(0, 2));
+  let cliff = parseInt(vd?.substring(2, 3));
+  if (vd?.substring(2, 3) == '1') {
+    period -= 12;
   }
-  let num=props.num
-  var newdata=[]
-  let aajdate=new Date().getFullYear() 
-  let aajyear=aajdate
-  let aajmonth=new Date().getMonth()
-  aajdate-=2000
-  aajdate*=12
-  aajdate+=aajmonth
+  let num = props.num
+  var newdata = []
+  let aajdate = new Date().getFullYear()
+  let aajyear = aajdate
+  let aajmonth = new Date().getMonth()
+  aajdate -= 2000
+  aajdate *= 12
+  aajdate += aajmonth
   for (let i = 0; i < data?.length; i++) {
     let currdate = data[i].date;
     let m = currdate?.substring(5, 7)
     let y = currdate?.substring(2, 4)
     let d = month[m - 1] + " " + y;
-    let nn=m+y;
+    let nn = m + y;
     data[i].md = d;
-    data[i].month=parseInt(m)
-    data[i].year=parseInt(y)
-    let yint=(parseInt(y)*12)+parseInt(m)
-    data[i].key=yint
-    data[i].n=aajdate-yint+1;
+    data[i].month = parseInt(m)
+    data[i].year = parseInt(y)
+    let yint = (parseInt(y) * 12) + parseInt(m)
+    data[i].key = yint
+    data[i].n = aajdate - yint + 1;
   }
   // console.log(props);
-  let count=0;
-  for(let i=0;i<data?.length;i++){
-    if(i==data?.length-1){
-      for(let j=data[i].n;j>=0;j--){
-        if(data[i].key<vs){
-        newdata.push({key:data[i].key,vp:0,price:data[i].price*num,name:data[i].name,n:j,month:data[i].month,year:data[i].year,md:data[i].md})
+  let count = 0;
+  for (let i = 0; i < data?.length; i++) {
+    if (i == data?.length - 1) {
+      for (let j = data[i].n; j >= 0; j--) {
+        if (data[i].key < vs) {
+          newdata.push({ key: data[i].key, vp: 0, price: data[i].price * num, name: data[i].name, n: j, month: data[i].month, year: data[i].year, md: data[i].md })
         }
-        else if(data[i].key>vs+period){
-        newdata.push({key:data[i].key,vp:data[i].price*num,price:data[i].price*num,name:data[i].name,n:j,month:data[i].month,year:data[i].year,md:data[i].md})
+        else if (data[i].key > vs + period) {
+          newdata.push({ key: data[i].key, vp: data[i].price * num, price: data[i].price * num, name: data[i].name, n: j, month: data[i].month, year: data[i].year, md: data[i].md })
         }
         else {
-          if(cliff==1&&data[i].key==vs){
-            count=12;
+          if (cliff == 1 && data[i].key == vs) {
+            count = 12;
           }
           else count++;
-        newdata.push({key:data[i].key,vp:num*data[i].price*count/totalp,price:data[i].price*num,name:data[i].name,n:j,month:data[i].month,year:data[i].year,md:data[i].md})
+          newdata.push({ key: data[i].key, vp: num * data[i].price * count / totalp, price: data[i].price * num, name: data[i].name, n: j, month: data[i].month, year: data[i].year, md: data[i].md })
 
         }
       }
     }
-    else{
-      for(let j=data[i].n;j>data[i+1].n;j--){
-        if(data[i].key<vs){
-          newdata.push({key:data[i].key,vp:0,price:data[i].price*num,name:data[i].name,n:j,month:data[i].month,year:data[i].year,md:data[i].md})
-          }
-          else if(data[i].key>vs+period){
-          newdata.push({key:data[i].key,vp:data[i].price*num,price:data[i].price*num,name:data[i].name,n:j,month:data[i].month,year:data[i].year,md:data[i].md})
-          }
-          else {
-            if(cliff==1&&data[i].key==vs){
-              count=12;
-            }
-            else count++;
-          newdata.push({key:data[i].key,vp:num*data[i].price*count/totalp,price:data[i].price*num,name:data[i].name,n:j,month:data[i].month,year:data[i].year,md:data[i].md})
-  
-          }
+    else {
+      for (let j = data[i].n; j > data[i + 1].n; j--) {
+        if (data[i].key < vs) {
+          newdata.push({ key: data[i].key, vp: 0, price: data[i].price * num, name: data[i].name, n: j, month: data[i].month, year: data[i].year, md: data[i].md })
         }
+        else if (data[i].key > vs + period) {
+          newdata.push({ key: data[i].key, vp: data[i].price * num, price: data[i].price * num, name: data[i].name, n: j, month: data[i].month, year: data[i].year, md: data[i].md })
+        }
+        else {
+          if (cliff == 1 && data[i].key == vs) {
+            count = 12;
+          }
+          else count++;
+          newdata.push({ key: data[i].key, vp: num * data[i].price * count / totalp, price: data[i].price * num, name: data[i].name, n: j, month: data[i].month, year: data[i].year, md: data[i].md })
+
+        }
+      }
     }
   }
-  count=0;
+  count = 0;
   // console.log(newdata);
-  
-  for(let i=0;i<newdata.length;i++){
-    let curr_month_no=newdata.length-i-1
-    let bmonth=curr_month_no%12
-    let byear=Math.floor(curr_month_no/12)
 
-    let cmonth=aajmonth
-    let cyear=aajyear-2000
-    if(bmonth>cmonth){
-      cmonth+=12
-      cmonth-=bmonth
+  for (let i = 0; i < newdata.length; i++) {
+    let curr_month_no = newdata.length - i - 1
+    let bmonth = curr_month_no % 12
+    let byear = Math.floor(curr_month_no / 12)
+
+    let cmonth = aajmonth
+    let cyear = aajyear - 2000
+    if (bmonth > cmonth) {
+      cmonth += 12
+      cmonth -= bmonth
       cyear--
     }
-    else{
-      cmonth-=bmonth
+    else {
+      cmonth -= bmonth
     }
-    cyear-=byear
+    cyear -= byear
 
     let d = month[cmonth] + " " + cyear;
 
-    if(newdata?.length)
-    newdata[i].md=d
-    let ckey=cyear*12+cmonth+1;
-    if(newdata?.length)
-    newdata[i].key=ckey
-    if(ckey<vs){
-      if(newdata?.length)
-      newdata[i].vp=0;
+    if (newdata?.length)
+      newdata[i].md = d
+    let ckey = cyear * 12 + cmonth + 1;
+    if (newdata?.length)
+      newdata[i].key = ckey
+    if (ckey < vs) {
+      if (newdata?.length)
+        newdata[i].vp = 0;
     }
-    else if(ckey>vs+period){
-      if(newdata?.length)
-      newdata[i].vp=newdata[i].price
+    else if (ckey > vs + period) {
+      if (newdata?.length)
+        newdata[i].vp = newdata[i].price
     }
-    else{
-      if(cliff==1&&ckey==vs){
-        count=13;
+    else {
+      if (cliff == 1 && ckey == vs) {
+        count = 13;
       }
       else count++;
-      if(newdata?.length)
-      newdata[i].vp=newdata[i].price*count/totalp
+      if (newdata?.length)
+        newdata[i].vp = newdata[i].price * count / totalp
     }
   }
-  while(newdata?.length>0&&newdata[1].vp==0){
-    if(cliff==1&&newdata[12].vp==0)
-    newdata.shift()
-    else if(cliff==0&&newdata[1].vp==0)
-    newdata.shift()
+  while (newdata?.length > 0 && newdata[1].vp == 0) {
+    if (cliff == 1 && newdata[12].vp == 0)
+      newdata.shift()
+    else if (cliff == 0 && newdata[1].vp == 0)
+      newdata.shift()
     else break;
   }
   let currentyearkey
   let finalprice
-  if(newdata.length)
-  {currentyearkey=newdata[newdata.length-1].key
-  finalprice=newdata[newdata.length-1].price}
-  let finalorprice
-  if(data)
-  {
-    finalorprice=data[data.length-1]?.price
+  if (newdata.length) {
+    currentyearkey = newdata[newdata.length - 1].key
+    finalprice = newdata[newdata.length - 1].price
   }
-  let finalyearkey=vs+period
+  let finalorprice
+  if (data) {
+    finalorprice = data[data.length - 1]?.price
+  }
+  let finalyearkey = vs + period
 
-  for(let i=currentyearkey;i<=finalyearkey+2;i++){
-    let imonth=i%12
-    let iyear=Math.floor(i/12)
+  for (let i = currentyearkey; i <= finalyearkey + 2; i++) {
+    let imonth = i % 12
+    let iyear = Math.floor(i / 12)
 
-    let imd=month[imonth]+" "+iyear
+    let imd = month[imonth] + " " + iyear
     // console.log(imd);
     count++
-    let ivp=finalorprice*count/totalp*num
-    if(ivp>finalprice)
-    ivp=finalprice
-    
-    newdata.push({key:i,vp:ivp,price:finalprice,name:i,n:i,month:imonth,year:iyear,md:imd})
+    let ivp = finalorprice * count / totalp * num
+    if (ivp > finalprice)
+      ivp = finalprice
+
+    newdata.push({ key: i, vp: ivp, price: finalprice, name: i, n: i, month: imonth, year: iyear, md: imd })
 
   }
-  console.log(aajdate);
-  for(let i=0;i<newdata.length;i++){
-    console.log(newdata[i].key);
-    if(newdata[i].key==aajdate+1){
-      newdata[i].vpt=newdata[i].vp
+  for (let i = 0; i < newdata.length; i++) {
+    if (newdata[i].key == aajdate + 1) {
+      newdata[i].vpt = newdata[i].vp
       break;
     }
   }
+  console.log(newdata);
+  if (variables?.time_to_ipo && variables?.ipo_price && variables?.lock_in) {
+
+    console.log(variables?.time_to_ipo);
+    if (newdata.length) {
+      console.log(parseInt(aajdate) + parseInt(variables?.time_to_ipo));
+      for (let currkey = finalyearkey + 1; currkey < (parseInt(aajdate) + parseInt(variables?.time_to_ipo))-2; currkey++) {
+        console.log(currkey);
+        let i = currkey+2
+        let imonth = i % 12
+        let iyear = Math.floor(i / 12)
+        let imd = month[imonth] + " " + iyear
+        newdata.push({ key: currkey, vp: newdata[newdata.length - 1]?.vp, price: newdata[newdata.length - 1]?.price, name: newdata[newdata.length - 1]?.name, n: newdata[newdata.length - 1]?.n + 1, month: imonth, year: iyear, md: imd })
+      }
+    }
+    let i=parseInt(aajdate) + parseInt(variables?.time_to_ipo)
+  let imonth = i % 12
+        let iyear = Math.floor(i / 12)
+        let imd = month[imonth] + " " + iyear
+  newdata.push({ key: (parseInt(aajdate) + parseInt(variables?.time_to_ipo))-2, vp: newdata[newdata.length - 1]?.vp, price:variables?.ipo_price*num , name: newdata[newdata.length - 1]?.name, n: newdata[newdata.length - 1]?.n + 1, month: imonth, year: iyear, md: imd })
+
+  for(let currkey=parseInt(aajdate) + parseInt(variables?.time_to_ipo)+1;currkey<parseInt(aajdate) + parseInt(variables?.time_to_ipo)+parseInt(variables?.lock_in)-2;currkey++){
+    let i = currkey+2
+        let imonth = i % 12
+        let iyear = Math.floor(i / 12)
+        let imd = month[imonth] + " " + iyear
+    newdata.push({ key: currkey, vp: newdata[newdata.length - 1]?.vp, price: newdata[newdata.length - 1]?.price, name: newdata[newdata.length - 1]?.name, n: newdata[newdata.length - 1]?.n + 1, month: imonth, year: iyear, md: imd })
+
+  }
+  for(let currkey=parseInt(aajdate) + parseInt(variables?.time_to_ipo)+parseInt(variables?.lock_in)-2;currkey<parseInt(aajdate) + parseInt(variables?.time_to_ipo)+parseInt(variables?.lock_in)+3;currkey++){
+    let i = currkey+2
+        let imonth = i % 12
+        let iyear = Math.floor(i / 12)
+        let imd = month[imonth] + " " + iyear
+    newdata.push({ key: currkey, vp: newdata[newdata.length - 1]?.price, price: newdata[newdata.length - 1]?.price, name: newdata[newdata.length - 1]?.name, n: newdata[newdata.length - 1]?.n + 1, month: imonth, year: iyear, md: imd })
+
+  }
+  }
+  
   // console.log(newdata);
   return (
     <ComposedChart
-      width={900}
+      width={800}
       height={400}
       data={newdata}
       margin={{
-        top: 10,
-        right: 30,
-        left: 100,
+        top: 0,
+        right: 0,
+        left: 50,
         bottom: 0
       }}
     >
@@ -248,7 +287,7 @@ export default function portfolioChart(props) {
         fill="#C9F0B1"
       />
       <Line type="monotone" dataKey="vp" stroke="#8884d8" strokeDasharray="5 5" />
-      <Scatter type="monotone" dataKey="vpt" fill="#8884d8" shape="dot"/>
+      <Scatter type="monotone" dataKey="vpt" fill="#8884d8" shape="dot" />
     </ComposedChart>
   );
 }
--- src/ZContainers/Portfolio/Portfolio.jsx
+++ src/ZContainers/Portfolio/Portfolio.jsx
@@ -2,38 +2,163 @@ import React, { useEffect, useState } from 'react'
 import PostfolioChart from '../../ZComponents/recharts/portfolioChart.jsx'
 import * as api from "../../axios.js"
 import { useNavigate } from 'react-router'
+import { Sidebar } from "react-responsive-sidebar"
+import Button from '../../Components/Button/Button.jsx'
+import monkey from "../../assets/monkey.svg"
+import { items } from '../../ZComponents/items.js'
 
 const Portfolio = () => {
-  const [cdetails,setcDetails]=useState()
-  const [chartdetails,setChartDetails]=useState()
+  const [cdetails, setcDetails] = useState()
+  const [chartdetails, setChartDetails] = useState()
   const navigate = useNavigate();
-    const user = localStorage.getItem("user");
-    const userobj = JSON.parse(localStorage.getItem('user'));
-    if (user === null) {
-        setTimeout(() => {
-            navigate("/auth");
-        }, 1000)
-    }
+  const user = localStorage.getItem("user");
+  const userobj = JSON.parse(localStorage.getItem('user'));
+  const [openlogout, setOpenlogout] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+
+  if (user === null) {
+    setTimeout(() => {
+      navigate("/auth");
+    }, 1000)
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsActive((current) => !current);
+  };
+  const [variables, setVariables] = useState({ email: userobj?.email })
+  const defaultClick = (e) => {
+    e.preventDefault();
+    setIsActive(false);
+  };
+  let logOut = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("user");
+    navigate("/auth");
+  };
   useEffect(() => {
-    async function f(){
-      let res=await api.getcData({c_name:"swiggy"})
+    async function f() {
+      let res = await api.getcData({ c_name: "swiggy" })
       setcDetails(res.data.obj)
     }
     f()
-    async function chartdata(){
-      let res=await api.getChartData({email:userobj?.email})
+    async function chartdata() {
+      let res = await api.getChartData({ email: userobj?.email })
       // console.log(res.data.message);
       setChartDetails(res.data.message)
-      
+
     }
     chartdata()
   }, [])
+
+  
   return (
-    <div>
-        <div className="mt-5 p-5">
-        {chartdetails&&<PostfolioChart data={cdetails} vd={chartdetails?.vesting_details} num={chartdetails?.num} vs={chartdetails?.vs} />}
+    <>
+      {user && <>
+        <div className="white-bg-css">
+          <Sidebar
+            className="side-bar z-s-i-css"
+            content={items}
+            width={200}
+            background={"#FFF"}
+            toggleIconColor={"#7B61FF"}
+            color={"#000000"}
+            activeHightlight={"#FFF"}
+            hoverHighlight={"#FFF"}
+            textAlign={"center"}
+          >
+            <div className="fix-nav ">
+              <div className="container">
+                <div className="row">
+                  <div className="col-9"></div>
+                  <div className="col-2 logo-top">
+                    <div className={openlogout ? "dropdown-monkey monkey-click" : "dropdown-monkey"}>
+                      <button className="monkey-btn-css" onClick={() => { setOpenlogout(current => !current) }}><img className="logo-top-size " src={monkey} /></button>
+                      <div className={openlogout ? "dropdown-content-monkey monkey-click" : "dropdown-content-monkey"}>
+                        <a style={{ textDecoration: "none" }} href="https://www.linkedin.com/company/zionn/" target="__blank"><Button name="contact" /></a>
+                        <button
+                          onPointerLeave={defaultClick}
+                          onPointerDown={handleClick}
+                          onPointerUp={handleClick}
+                          onClick={logOut}
+                          className={isActive ? "butt butt-ac logout-btn-css" : "butt logout-btn-css"}
+                        >
+                          logout&nbsp;
+                          <i class="bi bi-arrow-up-right"></i>
+                        </button>
+                      </div>
+                    </div>
+
+                  </div>
+                  <div className="col-1"></div>
+                </div>
+              </div>
+            </div>
+
+            <div onClick={() => setOpenlogout(false)} className="container con-abs">
+              <div className="row">
+                <div>
+                  <div className="container p-5 pt-0">
+                    <div className="row p-5 pt-0 ">
+                      <div className="col-10">
+                        <h3 className="pp-chirka" style={{ fontSize: "normal", fontWeight: "700", fontSize: "32px", lineHeight: "100%", textDecorationLine: "underline" }}>Portfolio</h3>
+                        <h3 className="pp-chirka mt-3" style={{ fontSize: "normal", fontWeight: "700", fontSize: "32px", lineHeight: "100%" }}>c name</h3>
+
+                      </div>
+                      <div className="row mt-3">
+                        <div className="col-3">
+                          <p className="pp-chirka mt-4" style={{ fontSize: "normal", fontWeight: "700", fontSize: "22px", lineHeight: "100%" }}>Quantity</p>
+                          <input className='new-input-css-2' style={{ width: "150px" }} type="number" value={chartdetails?.num} disabled />
+                        </div>
+                        <div className="col-3">
+                          <p className="pp-chirka mt-4" style={{ fontSize: "normal", fontWeight: "700", fontSize: "22px", lineHeight: "100%" }}>Strike Price</p>
+                          <input className='new-input-css-2' style={{ width: "150px" }} type="number" value={chartdetails?.strike_price} disabled />
+                        </div>
+                        <div className="col-3">
+                          <p className="pp-chirka mt-4" style={{ fontSize: "normal", fontWeight: "700", fontSize: "22px", lineHeight: "100%" }}>Vesting Start</p>
+                          <input className='new-input-css-2' style={{ width: "150px" }} type="text" value={chartdetails?.vesting_start_date} disabled />
+                        </div>
+                        <div className="col-3">
+                          <p className="pp-chirka mt-4" style={{ fontSize: "normal", fontWeight: "700", fontSize: "22px", lineHeight: "100%" }}>Latest Price</p>
+                          <input className='new-input-css-2' style={{ width: "150px" }} type="number" value={chartdetails?.fmp} disabled />
+                        </div>
+                      </div>
+                      <div className="row">
+                        <p className="pp-chirka mt-5" style={{ fontSize: "normal", fontWeight: "700", fontSize: "25px", lineHeight: "100%" }}>Variables</p>
+                        <div className="col-3">
+                          <p className="pp-chirka " style={{ fontSize: "normal", fontWeight: "700", fontSize: "22px", lineHeight: "100%" }}>Time to IPO</p>
+                          <input onChange={(e) => { setVariables({ ...variables, time_to_ipo: e.target.value }) }} className='new-input-css-2' style={{ width: "150px" }} type="number" />
+                        </div>
+                        <div className="col-3">
+                          <p className="pp-chirka" style={{ fontSize: "normal", fontWeight: "700", fontSize: "22px", lineHeight: "100%" }}>Lock-in</p>
+                          <input onChange={(e) => { setVariables({ ...variables, lock_in: e.target.value }) }} className='new-input-css-2' style={{ width: "150px" }} type="number" />
+                        </div>
+                        <div className="col-3">
+                          <p className="pp-chirka " style={{ fontSize: "normal", fontWeight: "700", fontSize: "22px", lineHeight: "100%" }}>IPO Price</p>
+                          <input onChange={(e) => { setVariables({ ...variables, ipo_price: e.target.value }) }} className='new-input-css-2' style={{ width: "150px" }} type="number" />
+                        </div>
+                        <div className="col-3">
+                          <p className="pp-chirka " style={{ fontSize: "normal", fontWeight: "700", fontSize: "22px", lineHeight: "100%" }}>Discount</p>
+                          <input onChange={(e) => { setVariables({ ...variables, discount: e.target.value }) }} className='new-input-css-2' style={{ width: "150px" }} type="number" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row p-5">
+                      <div style={{ backgroundColor: "#FEFCF7" }} className="col-10 p-5">
+                        {chartdetails && <PostfolioChart data={cdetails} vd={chartdetails?.vesting_details} variables={variables} num={chartdetails?.num} vs={chartdetails?.vs} />}
+
+                      </div>
+                      <div className="col-4"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Sidebar>
         </div>
-    </div>
+      </>}
+    </>
+
   )
 }
 
