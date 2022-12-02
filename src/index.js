8ae065018c3907bbbeff7a4248eaa517aee9d65f
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2022-12-02 19:50:43 +0530

    sentri

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+Sentry.init({
+  dsn: "https://7df1697275f44d0e9da7941c37a6ed68@o1385316.ingest.sentry.io/6705069",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
